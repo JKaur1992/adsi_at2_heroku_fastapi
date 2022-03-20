288421616d876fdbab026b50ff9e98e11d3f4e04
JKaur1992 <jasleen.kaur-1@student.uts.edu.au>
2022-03-20 23:53:47 +1100

    new pipeline model

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = FastAPI()
 
-model = load('../models/mlr_scaled_6Pred_NoChar_BothOrdinalEncoded_NADropped.joblib')
+model = load('../models/mlr_scaled_6Pred_Ordinal-Label_Pipeline.joblib')
 
 @app.get("/")
 def read_root():
