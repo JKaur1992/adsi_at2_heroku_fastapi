95f823199a4903307e98d018b8febd653abc9b47
JKaur1992 <jasleen.kaur-1@student.uts.edu.au>
2022-03-20 22:39:58 +1100

    new model

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = FastAPI()
 
-model = load('../models/mlr_scaled_6Pred_BothOrdinal_Pipeline.joblib')
+model = load('../models/mlr_scaled_6Pred_DropNA.joblib')
 
 @app.get("/")
 def read_root():
