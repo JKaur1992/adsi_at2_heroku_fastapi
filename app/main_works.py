c88db825c981dad6059884c4ea3880fa57c45017
JKaur1992 <jasleen.kaur-1@student.uts.edu.au>
2022-03-20 18:24:08 +1100

    run 5pred model with name API

--- app/main_works.py
+++ app/main_works.py
@@ -9,15 +9,14 @@ model = load('../models/mlr_scaled_5predictorsONLY.joblib')
 
 @app.get("/")
 def read_root():
-    return 'ML Model for Predicting Beer Style based on user rating criterias (API Expected Parameters) such as name, appearance, aroma, palate, taste and volume. Endpoints = /health/beer/type/prediction'
+    return 'ML Model for Predicting Beer Style based on user rating criterias (API Expected Parameters) such as Brewery Name AND Beer Appearance, Aroma, Palate, Taste and Volume. Endpoints = /health/beer/type/prediction. github repo link - https://github.com/JKaur1992/adsi_at2_heroku_fastapi'
 
 @app.get('/health', status_code=200)
 def healthcheck():
     return 'Multinomial Logistic Regression Model is all ready to go!'
 
-def format_features(name: str, aroma: float, appearance: float, palate: float, taste: float, volume: float):
+def format_features(aroma: float, appearance: float, palate: float, taste: float, volume: float):
     return {
-        'brewery_name': [name],
         'review_aroma': [aroma],
         'review_appearance': [appearance],
         'review_palate': [palate],
@@ -26,8 +25,8 @@ def format_features(name: str, aroma: float, appearance: float, palate: float, t
     }
     
 @app.get("/beer/type/prediction")
-def predict(name: str, aroma: float, appearance: float, palate: float, taste: float, volume: float):
-    features = format_features(name, aroma, appearance, palate, taste, volume)
+def predict(aroma: float, appearance: float, palate: float, taste: float, volume: float):
+    features = format_features(aroma, appearance, palate, taste, volume)
     variables = pd.DataFrame(features)
     pred = model.predict(variables)
     return JSONResponse(pred.tolist())
