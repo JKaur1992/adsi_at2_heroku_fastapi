94bb7c15f1e5f803c4b716f1eb86490406a9fe6b
JKaur1992 <jasleen.kaur-1@student.uts.edu.au>
2022-03-20 18:12:24 +1100

    used ordinal model

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = FastAPI()
 
-model = load('../models/mlr_scaled_6Pred_NoChar.joblib')
+model = load('../models/mlr_scaled_6Pred_BOTHOrdinalEn.joblib')
 
 @app.get("/")
 def read_root():
@@ -15,8 +15,9 @@ def read_root():
 def healthcheck():
     return 'Multinomial Logistic Regression Model is all ready to go!'
 
-def format_features(aroma: float, appearance: float, palate: float, taste: float, volume: float):
+def format_features(name: str, aroma: float, appearance: float, palate: float, taste: float, volume: float):
     return {
+        'brewery_name': [name],
         'review_aroma': [aroma],
         'review_appearance': [appearance],
         'review_palate': [palate],
@@ -25,8 +26,8 @@ def format_features(aroma: float, appearance: float, palate: float, taste: float
     }
     
 @app.get("/beer/type/prediction")
-def predict(aroma: float, appearance: float, palate: float, taste: float, volume: float):
-    features = format_features(aroma, appearance, palate, taste, volume)
+def predict(name: str, aroma: float, appearance: float, palate: float, taste: float, volume: float):
+    features = format_features(name, aroma, appearance, palate, taste, volume)
     variables = pd.DataFrame(features)
     pred = model.predict(variables)
     return JSONResponse(pred.tolist())
