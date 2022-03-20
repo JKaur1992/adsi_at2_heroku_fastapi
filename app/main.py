a75ab1e96cc6c9b61f98f611ed40b1fa8fd70ebf
JKaur1992 <jasleen.kaur-1@student.uts.edu.au>
2022-03-20 17:31:44 +1100

    added a new model

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = FastAPI()
 
-model = load('../models/mlr_scaled_6Pred_BOTHOrdinalEn.joblib')
+model = load('../models/mlr_scaled_6Pred_NoCharWS.joblib')
 
 @app.get("/")
 def read_root():
